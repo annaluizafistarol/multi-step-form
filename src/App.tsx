303b3c17ccd2b3fba46a17fc41ced2dd14a8a7b5
Anna Luiza de Camargo Fistarol <luizafistarol@gmail.com>
2025-11-18 18:10:34 -0300

    feat: wrap routes in FormProvider and adjust CSS variable values

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { JSX } from 'react'
+import FormProvider from '@utils/context/FormContext/FormContext'
 import Step1 from '@pages/step-1'
 import Step2 from '@pages/step-2'
 import Step3 from '@pages/step-3'
@@ -15,29 +16,31 @@ export default function App(): JSX.Element {
   return (
     <Router>
       <div className="app">
-        <Routes>
-          <Route path="/">
-            <Route index element={<Step1 />} />
-          </Route>
+        <FormProvider>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Step1 />} />
+            </Route>
 
-          <Route path="/step-1">
-            <Route index element={<Step1 />} />
-          </Route>
+            <Route path="/step-1">
+              <Route index element={<Step1 />} />
+            </Route>
 
-          <Route path="/step-2">
-            <Route index element={<Step2 />} />
-          </Route>
+            <Route path="/step-2">
+              <Route index element={<Step2 />} />
+            </Route>
 
-          <Route path="/step-3">
-            <Route index element={<Step3 />} />
-          </Route>
+            <Route path="/step-3">
+              <Route index element={<Step3 />} />
+            </Route>
 
-          <Route path="/step-4">
-            <Route index element={<Step4 />} />
-          </Route>
-        </Routes>
+            <Route path="/step-4">
+              <Route index element={<Step4 />} />
+            </Route>
+          </Routes>
 
-        <Box className="background-box" />
+          <Box className="background-box" />
+        </FormProvider>
       </div>
     </Router>
   )
